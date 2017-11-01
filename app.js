7b9bd812d7677bde9e3dcb293732426569e844d7
W. Logan Downing <30451101+Foxhound013@users.noreply.github.com>
2017-11-01 00:16:26 -0500

    Update to object
    Updated the object to be less complex and made use of the prototype method.

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 //IIFE in order to privatize the entire game.
-//(function () {
+(function () {
     var Question = function (questionText, answers, correctAnswer, pointValue) {
         this.questionText = questionText;
         this.answers = answers; //an array of answers
         this.correctAnswer = correctAnswer; //refers to the index of the correct answer in answers
-        //An object method for checking the users answer
-        this.checkAnswer = function(usersAnswer) {
-            if (Number(usersAnswer) === (currentQuestion.correctAnswer + 1)) {
-                console.log("===================");
-                console.log("Correct Answer!\n\n");
-                score += pointValue;
-                return(true); //continue the game
-            } else if (usersAnswer === "exit") {
-                console.log("Bye!");
-                return(false); //end the game
-            } 
-            else {
-                console.log("===================");
-                console.log("Better luck next time?");
-                return(true); //continue the game
-            }
-        };
-        this.pointValue = pointValue; //assign point values to each question
-        //An object method to display the score after each guess
-        this.displayScore = function() {
-            console.log("Your current score is: " + score + "\n\n");
-        };
+        this.pointValue = pointValue;
     };
 
+    //add in necessary methods to the objects prototype
+    //An object method to check the users answer
+    Question.prototype.checkAnswer = function (usersAnswer) {
+        if (Number(usersAnswer) === (currentQuestion.correctAnswer + 1)) {
+            console.log("===================");
+            console.log("Correct Answer!\n\n");
+            score += pointValue;
+            return(true); //continue the game
+        } else if (usersAnswer === "exit") {
+            console.log("Bye!");
+            return(false); //end the game
+        } else {
+            console.log("===================");
+            console.log("Better luck next time?");
+            return(true); //continue the game
+        }
+    };
+
+    //An object method to display the score after each guess
+    Question.prototype.displayScore = function () {
+        console.log("Your current score is: " + score + "\n\n");
+    };
 
     //instantiate the first question for the game
     var questionOne = new Question('What\'s better than JavaScript development?', ['1: Jesus', '2: Nothing', '3: PHP Development!'], 0, 5);
@@ -44,23 +45,21 @@
     var gameOn = true; //Allows the game to be shutdown if the player types exit (switches to false)
 
     while (gameOn === true) {
-        // randomization algorithm to randomize which question is selected follows the following pattern Math.floor(Math.random() * (max - min + 1)) + min;
-        var randQuestion = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+        // randomization algorithm to randomize which question is selected
+        var randQuestion = Math.floor(Math.random() * possibleQuestions.length);
         var currentQuestion = possibleQuestions[randQuestion]; //store the current question
         console.log(currentQuestion.questionText);
 
         //loop through the possible answers and display them to the player
         for (var i = 0; i < currentQuestion.answers.length; i++) {
             console.log(currentQuestion.answers[i]);
-        }
+        };
       
         var usersAnswer = prompt("Please select the correct answer (just type the number). Or type \"exit\" to quit.");
-      
-        
-
+    
         //check the answer, if the user enters exit then gameOn is set to false, otherwise it will be left as undefined.
         gameOn = currentQuestion.checkAnswer(usersAnswer);
         currentQuestion.displayScore();
     };
      
-//}) ();
+})();
